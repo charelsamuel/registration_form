2e4c431edb5dbac96031bba6a637815f1ec6ea74
charel <charelsamuel@gmail.com>
2019-11-23 15:50:50 +0700

    minor revision on registration.js

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,5 +1,17 @@
 $(document).ready(() => {
-    $.get('http://127.0.0.1:3000/test', (data) => {
-        console.log(data);
+    let url = 'http://127.0.0.1:3000/api/v1'; 
+    // $.get(url + '/account/test', (data) => {
+    //     console.log(data);
+    // });
+
+    $('#btn-register').click(() => {
+        let params = $("#form-register").serialize();
+        
+        $("#fieldset-register").attr("disabled", true);
+
+        $.post(url + '/account/register', params, (data) => {
+            console.log(data); 
+            $("#fieldset-register").attr("disabled", false);
+        });
     });
 });
