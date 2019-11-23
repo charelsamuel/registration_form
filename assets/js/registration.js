d317b8f8463d05535cdebae36a45c6265acf2aae
charel <charelsamuel@gmail.com>
2019-11-23 19:41:12 +0700

    add ability to show error on input

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,17 +1,18 @@
 $(document).ready(() => {
-    let url = 'http://127.0.0.1:3000/api/v1'; 
-    // $.get(url + '/account/test', (data) => {
-    //     console.log(data);
-    // });
+    let url = "http://127.0.0.1:3000/api/v1";
 
-    $('#btn-register').click(() => {
+    $("#btn-register").click(() => {
         let params = $("#form-register").serialize();
-        
-        $("#fieldset-register").attr("disabled", true);
 
-        $.post(url + '/account/register', params, (data) => {
-            console.log(data); 
+        $("#fieldset-register").attr("disabled", true);
+        
+        $.post(url + "/account/register", params, data => {
+            
+        }).fail(message => {
             $("#fieldset-register").attr("disabled", false);
-        });
+            $.each(message.responseJSON.attributes, (index, error) => {
+                $('#' + error.name).addClass('is-invalid');
+            })
+        }) ;
     });
 });
