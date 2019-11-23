ab035d477a816b41ddb06a278f253488c87ff69c
charel <charelsamuel@gmail.com>
2019-11-23 20:34:07 +0700

    add ability to get day, month, and year from server

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,5 +1,23 @@
 $(document).ready(() => {
     let url = "http://127.0.0.1:3000/api/v1";
+    
+    $.get(url + '/date/get-days', (data) => {
+        $.each(data, (index, day) => {
+            $('#birthDay').append(new Option(day, day, false, false));
+        });
+    })
+
+    $.get(url + '/date/get-months', (data) => {
+        $.each(data, (index, month) => {
+            $('#birthMonth').append(new Option(month.name, month.id, false, false));
+        });
+    })
+
+    $.get(url + '/date/get-years', (data) => {
+        $.each(data, (index, year) => {
+            $('#birthYear').append(new Option(year, year, false, false));
+        });
+    })
 
     $("#btn-register").click(() => {
         let params = $("#form-register").serialize();
